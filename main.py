b909b2359b3e55ca107e52bbce57396e919b84f3
nikhilyewalekar <nikhilyewalekar@gmail.com>
2025-12-27 12:13:41 +0530

    changes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask , Response, jsonify, request, render_template
 
+import sys
+import os
+sys.path.append(os.path.abspath("."))
 from src.utils import PredictExamScore
 
 app = Flask(__name__)
